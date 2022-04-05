ca1410fb6c7071e20652ac663486e1f3d6eb9cb0
Sergey Ostrovsky <sergey.ostrovsky@nordigy.ru>
2022-04-06 02:22:39 +0400

    [task2] persons count and maxAge fix

--- src/com/ostrovsky/Main.java
+++ src/com/ostrovsky/Main.java
@@ -9,11 +9,11 @@ public class Main {
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
         Collection<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10_000_000; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     families.get(new Random().nextInt(families.size())),
-                    new Random().nextInt(10_000_000),
+                    new Random().nextInt(100),
                     Sex.values()[new Random().nextInt(Sex.values().length)],
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
